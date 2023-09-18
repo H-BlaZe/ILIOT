184f88ccc93422c6b577d35e5c572e8428797b00
ibnAdam <100560353+H-BlaZe@users.noreply.github.com>
2023-09-18 13:29:15 +0530

    Add files via upload

---
+++ lib_cl_iliot.bat
@@ -0,0 +1,10 @@
+c:\
+pip install pytransform
+pip install pyarmour
+pip install os
+pip install time
+pip install pyfiglet
+pip install colorama
+pip keyboard
+echo CLOSE THE WINDOW AND OPEN TERMINAL IN THE FOLDER AND TYPE > python ILIOT.py
+pause
